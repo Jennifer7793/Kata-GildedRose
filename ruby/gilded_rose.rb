80bb1eaf251d561222cdd6487778880eb8e77bfa
Jennifer <jennifer.lin@abagile.com>
2022-10-05 10:15:51 +0800

    Trying to use 三元運算子 but still no idea how to tidy up with it

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -11,6 +11,7 @@ class GildedRose
         item.quality += 1 if item.quality < 50
         item.sell_in -= 1
         item.quality += 1 if item.quality < 50 && item.sell_in < 0
+        item.quality = item.quality > 50 ? 50 : item.quality
       when 'Backstage passes to a TAFKAL80ETC concert'
         item.quality += 1 if item.quality < 50
         item.quality += 1 if item.quality < 50 && item.sell_in < 11
